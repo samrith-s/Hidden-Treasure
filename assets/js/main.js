810ca8b1f3a8925d79d627fec4b24f3b41cf1ef8
saurabhbhandari2006 <saurabhbhandari2006@gmail.com>
2014-11-03 16:35:50 +0530

    commited

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(function() {
+    $("#countDown").countdowntimer({
+        minutes: 2,
+        size: "lg",
+        tickInterval: 1
+    });
+
+function timeisUp(){
+    //Code to be executed when timer expires.
+}
+});
