51c4185d4c130138c4badf6ecbaf4868cbf17647
agarwalmehul <agarwalmehul92@gmail.com>
2014-11-03 14:13:17 +0530

    minor functions done

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,3 +1,4 @@
+window.theme = {};
 
 var item = [
     {
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,18 @@
 
 var objectHash;
-var find;
+var find = [];
+var clicked;
 
 
-function initThem() {
+function initTheme() {
 
+}
+
+function createFindList() {
+    objectHash = shuffle(objects);
+    for(var i = 0; i < 10; i++) {
+        find.push(objectHash[i]);
+    }
 }
 
 function initPage() {
@@ -12,18 +20,51 @@ function initPage() {
 }
 
 function initGame() {
+    clicked = false;
 
+    $("#countdown").countdowntimer({
+        minutes: 2,
+        tickInterval: 1,
+        size: "lg",
+        timeSeparator: ":",
+        timeUp: endGame()
+    });
 }
 
 function getClick() {
+    $(".true").on('click', function() {
+        processClick($(this).attr("id"));
+    });
+}
+
+function processClick(id) {
 
 }
 
-function processClick() {
+function showSplash(msg, delay, callback) {
+    switch(delay.toString()) {
+        case "0":
+            msg += msg += " <br /> <br /> <input value='Play Again' type='button' onclick='initTheme()' /> ";
+            $('#splasher').html(msg).fadeIn(500);
+            break;
 
+        default:
+            $('#splasher').html(msg).fadeIn(500).delay(delay).fadeOut(500);
+            setTimeout(function() {
+                if(typeof callback === "function") {
+                    callback();
+                }
+            }, 3000);
+    }
+}
+
+function victory() {
+    var msg = "Congratulations! <br /> You found all the Hidden Objects! <br /> <br/> Click 'Play Again' to Restart the Game"
+    showSplash(msg, 0);
 }
 
 function endGame() {
-
+    var msg = "Uh Oh! <br /> Time's Up! <br /> <br/> Click 'Play Again' to Restart the Game"
+    showSplash(msg, 0);
 }
 
